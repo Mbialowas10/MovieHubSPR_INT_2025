2ca90d9b76e6ed9840ddb43f4ccee5763dc53c14
Michael Bialowas <mbialowas@rrc.ca>
2025-05-22 09:53:03 -0500

    feature: wired up bottomNavigationBar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,7 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.navigation.compose)
+    implementation("androidx.navigation:navigation-compose:2.8.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/mbialowas/moviehubspr_int_2025/Navigation/Navigation.kt
+++ app/src/main/java/com/mbialowas/moviehubspr_int_2025/Navigation/Navigation.kt
@@ -1,12 +1,56 @@
 package com.mbialowas.moviehubspr_int_2025.Navigation
 
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.mbialowas.moviehubspr_int_2025.R
+import com.mbialowas.moviehubspr_int_2025.destinations.Destination
+import kotlinx.coroutines.selects.select
 
 @Composable
 fun BottomNav(navController: NavController){
     NavigationBar {
+        val navBackStackEntry = navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry.value?.destination
 
+        val ic_movie = painterResource(id=R.drawable.ic_movie)
+        val ic_watch = painterResource(id = R.drawable.ic_watch)
+        val ic_search = painterResource(id=R.drawable.ic_search)
+
+        NavigationBarItem(
+            selected = currentDestination?.route == Destination.Movie.route,
+            onClick = {
+                navController.navigate(Destination.Movie.route){
+                    popUpTo(Destination.Movie.route)
+                    launchSingleTop = true
+                }},
+                icon = { Icon(painter = ic_movie, contentDescription = null)},
+                label = { Text(text= Destination.Movie.route) }
+        ) // end movie
+        NavigationBarItem(
+            selected = currentDestination?.route == Destination.Search.route,
+            onClick = {
+                navController.navigate(Destination.Search.route){
+                    popUpTo(Destination.Search.route)
+                    launchSingleTop = true
+                }},
+            icon = { Icon(painter = ic_search, contentDescription = null)},
+            label = { Text(text= Destination.Search.route) }
+        ) // end search
+        NavigationBarItem(
+            selected = currentDestination?.route == Destination.Watch.route,
+            onClick = {
+                navController.navigate(Destination.Watch.route){
+                    popUpTo(Destination.Watch.route)
+                    launchSingleTop = true
+                }},
+            icon = { Icon(painter = ic_watch, contentDescription = null)},
+            label = { Text(text= Destination.Watch.route) }
+        ) // end watch
     }
 }

d9428b70c0a79081b196d148214fa7ac8deef739
Michael Bialowas <mbialowas@rrc.ca>
2025-05-26 13:47:58 -0500

    movie data is accessible on MovieDetail page

--- app/src/main/java/com/mbialowas/moviehubspr_int_2025/screens/MovieDetailScreen.kt
+++ app/src/main/java/com/mbialowas/moviehubspr_int_2025/screens/MovieDetailScreen.kt
@@ -3,10 +3,14 @@ package com.mbialowas.moviehubspr_int_2025.screens
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import com.mbialowas.moviehubspr_int_2025.api.model.Movie
 
 @Composable
@@ -19,5 +23,26 @@ fun MovieDetailScreen(modifier: Modifier, movie: Movie){
         Log.i("MovieDetailScreen","${movie.title}")
         Log.i("MovieDetailScreen","${movie.overview}")
         Log.i("MovieDetailScreen","${movie.posterPath}")
+
+        Column{
+            Text(
+                text = movie.title!!,
+                color = Color.Black,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = movie.overview!!,
+                color = Color.Black,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = movie.posterPath!!,
+                color = Color.Black,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
